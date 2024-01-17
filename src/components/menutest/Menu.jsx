bc571dfcecaec14f64f12c42a2ba179323894df3
nicolasjrz <nicolasjrz.dev@gmail.com>
2024-01-17 18:31:13 -0300

    test new menu 3d page

---
+++ src/components/menutest/Menu.jsx
@@ -0,0 +1,229 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+
+import Button from "@mui/material/Button";
+
+import logo from "../../assets/logo2.png";
+
+const pages = [
+  { id: 1, name: "Parallax", url: "/" },
+  { id: 2, name: "Skewed", url: "/skewed" },
+  { id: 3, name: "Gallery", url: "/gallery" },
+  { id: 4, name: "3D", url: "/3D" },
+];
+
+import { Link, Link as RouterLink } from "react-router-dom";
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{
+        textAlign: "center",
+        // transition: "22s", //
+        // transitionDelay: "0.3s",
+        // transitionDuration: "43s",
+      }}
+      //   bgcolor={"violet"}
+      display={"flex"}
+      height={"100%"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      {/* <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider /> */}
+      <List>
+        {pages.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <Link
+                component={RouterLink}
+                to={item.url}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <ListItemText primary={item.name} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+        {/* {pages.map((page) => (
+                <MenuItem key={page.id} onClick={handleCloseNavMenu}>
+                  <Link
+                    component={RouterLink}
+                    to={page.url}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </Link>
+                </MenuItem>
+              ))} */}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar
+          sx={{
+            bgcolor: "red",
+            justifyContent: "space-between",
+            justifyItems: "center",
+          }}
+        >
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+          {/* <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              //   display: { xs: "flex", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              //   letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography> */}
+          <img
+            src={logo} // Reemplaza con la ruta real de tu logo
+            alt="Logo"
+            style={{ maxWidth: 150, maxHeight: 100 }} // Ajusta el tamaño según tus necesidades
+          />
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{
+              mr: 2,
+              display: { sm: "none" },
+              // bgcolor: "yellow"//
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {pages.map((item) => (
+              <>
+                <Link
+                  component={RouterLink}
+                  to={item.url}
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  {/* <ListItemText primary={item.name} /> */}
+                  <Button key={item.id} sx={{ color: "#fff" }}>
+                    {item.name}
+                  </Button>
+                </Link>
+              </>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          anchor="top"
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: "100%",
+              height: "100%",
+              //   transitionDuration: 4, // Puedes ajustar este valor según tus preferencias
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      {/* <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
+          quibusdam, aliquam dolore excepturi quae. Distinctio enim at eligendi
+          perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod
+          itaque exercitationem, at ab sequi qui modi delectus quia corrupti
+          alias distinctio nostrum. Minima ex dolor modi inventore sapiente
+          necessitatibus aliquam fuga et. Sed numquam quibusdam at officia
+          sapiente porro maxime corrupti perspiciatis asperiores, exercitationem
+          eius nostrum consequuntur iure aliquam itaque, assumenda et! Quibusdam
+          temporibus beatae doloremque voluptatum doloribus soluta accusamus
+          porro reprehenderit eos inventore facere, fugit, molestiae ab officiis
+          illo voluptates recusandae. Vel dolor nobis eius, ratione atque
+          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis,
+          voluptatem! Cumque, eligendi unde aliquid minus quis sit debitis
+          obcaecati error, delectus quo eius exercitationem tempore. Delectus
+          sapiente, provident corporis dolorum quibusdam aut beatae repellendus
+          est labore quisquam praesentium repudiandae non vel laboriosam quo ab
+          perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
+          commodi nihil corrupti cum non fugiat praesentium doloremque
+          architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
+          molestiae dicta blanditiis est expedita eius debitis cupiditate porro
+          sed aspernatur quidem, repellat nihil quasi praesentium quia eos,
+          quibusdam provident. Incidunt tempore vel placeat voluptate iure
+          labore, repellendus beatae quia unde est aliquid dolor molestias
+          libero. Reiciendis similique exercitationem consequatur, nobis placeat
+          illo laudantium! Enim perferendis nulla soluta magni error, provident
+          repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
+          suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
+          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore
+          commodi reprehenderit rerum reiciendis! Quidem alias repudiandae eaque
+          eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
+          ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat
+          rem repellendus. Voluptates perspiciatis, in pariatur impedit, nam
+          facilis libero dolorem dolores sunt inventore perferendis, aut
+          sapiente modi nesciunt.
+        </Typography>
+      </Box> */}
+    </Box>
+  );
+}
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
---
+++ src/pages/TresDePage.jsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+import DrawerAppBar from "../components/menutest/Menu";
+
+export const TresDePage = () => {
+  return (
+    <>
+      <DrawerAppBar />
+      <Grid container>
+        <Grid item>TresDePage</Grid>
+      </Grid>
+    </>
+  );
+};
